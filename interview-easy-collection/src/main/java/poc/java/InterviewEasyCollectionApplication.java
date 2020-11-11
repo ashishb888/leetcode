8ab7cfddcfc176ca0257ecfed6cbfe699309fba6
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-11 10:23:46 +0530

    interview-easy-collection: Initial commit

---
+++ interview-easy-collection/src/main/java/poc/java/InterviewEasyCollectionApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewEasyCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewEasyCollectionApplication.class, args);
+	}
+
+}
