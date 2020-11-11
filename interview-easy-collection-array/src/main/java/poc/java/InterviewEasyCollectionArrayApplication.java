f148f8ad25c7ce935a56edd0baefeb7da1b18c54
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-11 10:28:31 +0530

    interview-easy-collection-array: Initial commit

---
+++ interview-easy-collection-array/src/main/java/poc/java/InterviewEasyCollectionArrayApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewEasyCollectionArrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewEasyCollectionArrayApplication.class, args);
+	}
+
+}
---
+++ interview-easy-collection-array/src/test/java/poc/java/InterviewEasyCollectionArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewEasyCollectionArrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
