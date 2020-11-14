dd12762f6377f9c1795074d60349b83a23ef5415
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-14 20:01:02 +0530

    binary-search: Initial commit

---
+++ binary-search/src/main/java/poc/java/BinarySearchApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarySearchApplication.class, args);
+	}
+
+}
