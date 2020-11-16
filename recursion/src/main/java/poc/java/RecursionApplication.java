289238e6fc4f38b24dd595e9eaba412cc47cda47
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-16 19:17:31 +0530

    recursion: Initial commit

---
+++ recursion/src/main/java/poc/java/RecursionApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursionApplication.class, args);
+	}
+
+}
---
+++ recursion/src/test/java/poc/java/RecursionApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
