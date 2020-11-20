a4e5ab540ad49e853c2b87f98f998be951df21e2
ashishb888 <ashish.bhosle008@gmail.com>
2020-11-20 11:07:03 +0530

    interview-easy-collection-linkedlist: Initial commit

---
+++ interview-easy-collection-linkedlist/src/main/java/poc/java/InterviewEasyCollectionLinkedlistApplication.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewEasyCollectionLinkedlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewEasyCollectionLinkedlistApplication.class, args);
+	}
+
+}
