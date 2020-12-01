9bec797c55c425f10e5ab7e0d5da44dd3aec4b29
ashishb888 <ashish.bhosle008@gmail.com>
2020-12-01 09:16:02 +0530

    interview-medium-collection-array: Initial commit

---
+++ interview-medium-collection-array/src/test/java/poc/java/InterviewMediumCollectionArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMediumCollectionArrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
